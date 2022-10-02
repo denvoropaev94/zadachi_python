f5261b07414607ebfde340376d478f1e39dc2e33
«Denis» <denvoropaev7@gmail.com>
2022-10-02 13:55:54 +0300

    Программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.

---
+++ task_1_5.py
@@ -0,0 +1,10 @@
+# 5. Напишите программу, которая принимает на вход координаты
+#    двух точек и находит расстояние между ними в 2D пространстве.
+#    https://ru.onlinemschool.com/math/library/analytic_geometry/point_point_length/
+
+x_1 = int(input())
+y_1 = int(input())
+x_2 = int(input())
+y_2 = int(input())
+
+print(f"{((x_2 - x_1) ** 2 + (y_2 - y_1) ** 2) ** 0.5:0.4}")
