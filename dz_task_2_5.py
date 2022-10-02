5a3765dfa8f4f6bb49f1387a4dafda2fb23f0121
«Denis» <denvoropaev7@gmail.com>
2022-10-02 14:23:56 +0300

    алгоритм перемешивания списка.Без функции shuffle из модуля random

---
+++ dz_task_2_5.py
@@ -0,0 +1,16 @@
+# 5. **Реализуйте алгоритм перемешивания списка.
+#    Без функции shuffle из модуля random.
+
+from random import randrange
+
+num = int(input())
+nums_list = list(range(num))
+res_list = []
+
+print(nums_list)
+
+for i in range(num):
+    n_1, n_2 = randrange(num), randrange(num)
+    nums_list[n_1], nums_list[n_2] = nums_list[n_2], nums_list[n_1]
+
+print(nums_list)
